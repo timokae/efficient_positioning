4aab0bf8bc22311984aead184810c404b0148ff8
AlexanderMiller <alexandermiller1996@yahoo.de>
2019-01-04 17:33:46 +0100

    GUI + Grundgerüst
    Gui erstellt und Logik- Grundgerüst + locationmanagerChange Funktion

---
+++ app/src/main/java/com/example/team919/efficient_positioning/MainActivity.java
@@ -0,0 +1,204 @@
+package com.example.team919.efficient_positioning;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.LocationProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+
+
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_FINE_LOCATION = 0;
+
+    LocationListener locationListener;
+    private LocationManager locationManager;
+    private String locationProvider;
+    final Context context = this;
+
+    long minTime = 0;
+    float minDistance = 0;
+    double meterProSekunde = 0.0;
+
+    EditText editDistanz;
+    EditText editMinZeit;
+    EditText editMS;
+    RadioGroup radioGroup;
+    RadioButton radioDistanz;
+    RadioButton radioMinZeit;
+    RadioButton radioMS;
+    RadioButton radioStill;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editDistanz  = findViewById(R.id.editDistanz);
+        editMinZeit  = findViewById(R.id.editMinZeit);
+        editMS =findViewById(R.id.editMS);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioMinZeit = findViewById(R.id.radioMinZeit);
+        radioDistanz = findViewById(R.id.radioDistanz);
+        radioMS = findViewById(R.id.radioMS);
+        radioStill =findViewById(R.id.radioStill);
+
+        if (!fine_location_permitted()) {
+            permit_fine_location(MY_PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        locationListener = getLocation();
+        locationProvider = LocationManager.GPS_PROVIDER;
+
+        //Distanz einstellen ---------------------------------------------------------------------
+        editDistanz.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                minDistance = Float.parseFloat(editDistanz.getText().toString());
+            }
+        });
+
+
+        //minimale Zeit einstellen ---------------------------------------------------------------------
+        editMinZeit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                minDistance = Float.parseFloat(editMinZeit.getText().toString());
+            }
+        });
+
+        //Meter pro Sekunde einstellen ---------------------------------------------------------------------
+        editMS.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                minDistance = Float.parseFloat(editMS.getText().toString());
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.radioDistanz:
+                        Toast.makeText(context, "Hallo Distanz", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.radioMinZeit:
+                        Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+
+                        break;
+
+                    case R.id.radioMS:
+
+                        Toast.makeText(context, editMinZeit.getText(), Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.radioStill:
+                        break;
+
+                }
+            }
+        });
+
+    }
+
+
+
+
+    void setLocationUpdatesTime(long minTime){
+        locationManager.removeUpdates(locationListener);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(locationProvider,minTime,0,locationListener);
+    }
+
+    private LocationListener getLocation(){
+        // Hier die Anwednung Starten Später noch aus Lagern
+        //Toast.makeText(getApplicationContext(),"Berechtigung erteilt",Toast.LENGTH_LONG).show();
+        // LocationListener erstellen
+
+        return new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+
+        };
+
+    }
+
+    // Fine Location Permission
+    private boolean fine_location_permitted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void permit_fine_location(int callback) {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, callback);
+    }
+
+}
